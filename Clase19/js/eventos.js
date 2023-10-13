01c4002efd6b248d3fbdc76e9a7bc1877666fc49
nicolasfernandez4 <nicolas.fernandez4@bue.edu.ar>
2023-10-12 22:13:46 -0300

    Clase19

---
+++ Clase19/js/eventos.js
@@ -0,0 +1,17 @@
+function publicidad()
+{
+ alert("Muestro una publicidad");
+}
+
+
+function cambiar(fondo,letra)
+{
+    document.getElementById("texto").style.backgroundColor=fondo;
+    document.getElementById("texto").style.color=letra;
+}
+
+function cambiarFondo(fondo)
+{
+    document.getElementById("textoInput").style.backgroundColor=fondo;
+
+}
